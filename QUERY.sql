c369688802b6439804be5c9c41e4a0615ace095d
Shajidaa <shajidaakterlopa101@gmail.com>
2026-06-14 20:45:29 +0600

    query 1 done

--- QUERY.sql
+++ QUERY.sql
@@ -183,7 +183,13 @@ VALUES
 
 
 --Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
-
+SELECT 
+    match_id, 
+    fixture, 
+    base_ticket_price
+FROM matches
+WHERE tournament_category = 'Champions League' 
+  AND match_status = 'Available';
 
 
 --Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
